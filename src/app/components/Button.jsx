3ca1ec02cee36368dab6d0f6637360b352e0c456
samsonkassa-dev <samsonkassa25@gmail.com>
2024-04-13 16:45:41 +0300

    button hover background

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,4 +1,4 @@
 const Button = ({ text }) => (
-    <button className="bg-[#FFCF23] text-black w-full text-[14px] rounded py-2 px-5 ">{text}</button>
+    <button className="bg-[#FFCF23] hover:bg-[#fed84c] text-black w-full text-[14px] rounded py-2 px-5 ">{text}</button>
   );
 export default Button  
